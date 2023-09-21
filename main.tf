65aa8abb9851b568ef1901b3a5fb4aee7c6fa83a
Srikanth Anantharamu <srikanth.anantharamu@outlook.com>
2023-09-21 22:38:01 +0530

    Updated main.tf file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAS2SMN4NZWTPTMS5U"
-  secret_key = "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"
+  access_key = "AKIAS2SMN4NZ43O73FM5"
+  secret_key = "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"
 }
 
 resource "aws_instance" "my-first-ubuntu-server" {
